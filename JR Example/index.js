ddd550cd0cc08ec703d17395c96e69b86db3470b
mrNateDog <nathan.mcclure@gmail.com>
2021-12-21 15:04:46 -0600

    Init comit

---
+++ JR Example/index.js
@@ -0,0 +1,78 @@
+$(document).ready(function () {
+  $("#submit").click(function (e) {
+    var validate = Validate();
+    let cityId = $("#citySelect").val(); 
+    $("#message").html(validate);
+    if (validate.length == 0) {
+      $.ajax({
+        type: "GET",
+        url:
+          "http://api.openweathermap.org/data/2.5/weather?id=" +
+          $("#citySelect").val() +
+          "&appid=cc99add73664c7a3af4a28dc0b848471",
+        data: {
+          id: cityId,
+        },
+        dataType: "json",
+        success: function (result, status, xhr) {
+          console.log(result);
+          var table = $("<table><tr><th>Weather Description</th></tr>");
+
+          table.append(
+            "<tr><td>City:</td><td>" + result["name"] + "</td></tr>"
+          );
+          table.append(
+            "<tr><td>Country:</td><td>" +
+              result["sys"]["country"] +
+              "</td></tr>"
+          );
+          table.append(
+            "<tr><td>Current Temperature:</td><td>" +
+              result["main"]["temp"] +
+              "°C</td></tr>"
+          );
+          table.append(
+            "<tr><td>Humidity:</td><td>" +
+              result["main"]["humidity"] +
+              "</td></tr>"
+          );
+          table.append(
+            "<tr><td>Weather:</td><td>" +
+              result["weather"][0]["description"] +
+              "</td></tr>"
+          );
+
+          $("#message").html(table);
+        },
+        error: function (xhr, status, error) {
+          alert(
+            "Result: " +
+              status +
+              " " +
+              error +
+              " " +
+              xhr.status +
+              " " +
+              xhr.statusText
+          );
+        },
+      });
+    }
+  });
+
+  $(document).ajaxStart(function () {
+    $("img").show();
+  });
+
+  $(document).ajaxStop(function () {
+    $("img").hide();
+  });
+
+  function Validate() {
+    var errorMessage = "";
+    if ($("#citySelect").val() == "Select") {
+      errorMessage += "► Select City";
+    }
+    return errorMessage;
+  }
+});
